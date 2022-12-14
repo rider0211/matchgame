d500321f0dd89138c36225e782b5d52e7fd57d9b
rider0211 <codemaster9428@gmail.com>
2022-12-14 13:42:11 -0600

    commit second

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
-  $(".search").click(function(){
-    $(this).hide();
-    $(".search_input").show();
-  });
-  $(".search_input").mouseleave(function(){
-    $(this).hide();
-    $(".search").show();
-  });
+  // $(".search").click(function(){
+  //   $(this).hide();
+  //   $(".search_input").show();
+  // });
+  // $(".search_input").mouseleave(function(){
+  //   $(this).hide();
+  //   $(".search").show();
+  // });
+  $(".frame15").click(function(){
+    $(".frame15").removeClass("frame15_active");
+    $(this).addClass('frame15_active');
+  })
 });
